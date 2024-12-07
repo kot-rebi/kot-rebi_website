84d42197bc5b74273e48555c7c2d81ccf63ca430
kot-rebi <kotrebi9@gmail.com>
2024-12-07 11:28:15 +0900

    refactor: views/homeのファイルをconfig.phpで読み込むように修正 #1
    article.php,index.phpのインデント整理とincludeのパス修正

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="ja">
 
-<?php include __DIR__ . '/../shared/head.php'; ?>
+<?php
+require_once __DIR__ . '/../../../config.php';
+include GLOBAL_SHARED_PATH . '/head.php';
+?>
 
 <body>
-  <?php 
-  require_once __DIR__ . '/../../functions.php';
-  include __DIR__ . '/../shared/header.php';
+  <?php
+  require_once FUNCTIONS_PATH;
+  include GLOBAL_SHARED_PATH . 'header.php';
   ?>
 
   <!-- コンテンツ -->
@@ -15,29 +18,30 @@
     <!-- メインコンテンツ -->
     <main>
       <div class="main__content">
-      <?php foreach ($articles as $article): ?>
-        <article class="content-card">
-          <div class="content-card__body">
-            <img src="/pj_homepage/assets/image/img_ranking1.jpg" class="content-card__image">
-            <div class="content--card__date-wrapper">
-              <i class="fa-regular fa-clock conten-card__icon"></i>
-              <p class="content-card__date"><?= $article['updated_at'] ?></p>
+        <?php foreach ($articles as $article): ?>
+          <article class="content-card">
+            <div class="content-card__body">
+              <img src="/pj_homepage/assets/image/img_ranking1.jpg" class="content-card__image">
+              <div class="content--card__date-wrapper">
+                <i class="fa-regular fa-clock conten-card__icon"></i>
+                <p class="content-card__date"><?= $article['updated_at'] ?></p>
+              </div>
+              <p class="content--card__title"><?= $article['title'] ?></p>
             </div>
-            <p class="content--card__title"><?= $article['title'] ?></p>
-          </div>
-        </article>
+          </article>
         <?php endforeach; ?>
       </div>
       <div class="pagination">
-          <a href="<?= $currentPage == 1 ? '#' : '?page=' . ($currentPage - 1) ?>" class="<?= $currentPage == 1 ? 'disabled' : '' ?>">前へ</a>
-          <?php for ($i = 1; $i <= $totalPages; $i++) : ?>
-            <a href="?page=<?= $i ?>" class="<?= $i == $currentPage ? 'active' : '' ?>"><?= $i ?></a>
-          <?php endfor; ?>
-          <a href="<?= $currentPage == $totalPages ? '#' : '?page=' . ($currentPage + 1) ?>" class="<?= $currentPage == $totalPages ? 'disabled' : '' ?>">次へ</a>
-        </div>
+        <a href="<?= $currentPage == 1 ? '#' : '?page=' . ($currentPage - 1) ?>" class="<?= $currentPage == 1 ? 'disabled' : '' ?>">前へ</a>
+        <?php for ($i = 1; $i <= $totalPages; $i++) : ?>
+          <a href="?page=<?= $i ?>" class="<?= $i == $currentPage ? 'active' : '' ?>"><?= $i ?></a>
+        <?php endfor; ?>
+        <a href="<?= $currentPage == $totalPages ? '#' : '?page=' . ($currentPage + 1) ?>" class="<?= $currentPage == $totalPages ? 'disabled' : '' ?>">次へ</a>
+      </div>
     </main>
-    <?php include __DIR__ . '/../home/shared/sidebar.php'; ?>
+    <?php include HOME_SHARED_PATH . 'sidebar.php'; ?>
   </div>
-<?php include __DIR__ .  '/../home/shared/footer.php' ?>
+  <?php include HOME_SHARED_PATH .  'footer.php' ?>
 </body>
+
 </html>
