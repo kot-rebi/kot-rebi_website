5a773b30dee48812adc81aeeb24c66ed4d8c793b
kot-rebi <kotrebi9@gmail.com>
2024-12-09 18:15:31 +0900

    feat: 新規作成するときにサムネイル画像も送信する機能を追加 #5

--- app/controllers/CreateArticleController.php
+++ app/controllers/CreateArticleController.php
@@ -10,9 +10,7 @@ class CreateArticleController extends BaseArticleController
   {
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
       $this->displayCreateForm();
-    }
-
-    else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       $this->insertArticle();
     }
   }
@@ -29,24 +27,56 @@ class CreateArticleController extends BaseArticleController
       'articleId' => $this->articleId
     ];
     extract($viewData);
-    
+
     include VIEWS_ADMIN_PATH . 'createArticle.php';
   }
 
   private function insertArticle()
   {
     $data = $this->getInputData();
+    $thumbnailData = null;
 
+    // サムネイル画像の仮保存
+    if (!empty($_FILES['thumbnail']['tmp_name'])) {
+      $thumbnailInfo = $this->uploadThumbnail($_FILES['thumbnail']);
+      if ($thumbnailInfo) {
+        $thumbnailData = [
+          'tmp_path' => $thumbnailInfo['tmp_path'],
+          'file_name' => basename($thumbnailInfo['url_path']),
+          'file_path' => $thumbnailInfo['url_path'],
+          "alt_text" => $data['title'],
+        ];
+      }
+    }
+
+    if ($thumbnailData === null) {
+      echo "サムネイル画像が必要です";
+      return;
+    }
+
+    // 記事挿入処理
     if ($this->validateArticleSave($data)) {
-      $this->articleModel->insertArticles($data['title'], $data['content']);
+      $articleId = $this->articleModel->insertArticles($data['title'], $data['content'], $thumbnailData);
+      echo $articleId;
+      if (!$articleId) {
+        echo "記事の挿入に失敗しました";
+        return;
+      }
+
+      // サムネイル画像のリネーム
+      if ($thumbnailData && $articleId) {
+        $newFileName = IMAGE_UPLOADS_THUMBNAILS_PATH . 'thumbnail_' . $articleId . '.' . pathinfo($thumbnailData['file_name'], PATHINFO_EXTENSION);
+        rename($thumbnailData['tmp_path'], $newFileName);
+      }
+
       header("Location:" . ADMIN_ARTICLES_URL);
       exit;
     } else {
-      echo "入力内容にエラーがありました";
+      echo "記事の保存に失敗しました";
     }
   }
 
-    /**
+  /**
    * 新規作成用のviewの変数をセットする
    * 
    * @return void
