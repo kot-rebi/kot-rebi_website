4cd5df1fc6ad155e9bcc1172326d0c2432b9d1a8
kot-rebi <kotrebi9@gmail.com>
2025-01-14 22:20:15 +0900

    feat: 管理画面の記事一覧全体のデザイン変更及び状態管理 #23

--- app/views/admin/ArticleList.php
+++ app/views/admin/ArticleList.php
@@ -19,8 +19,8 @@ include GLOBAL_SHARED_PATH . '/head.php';
 
   <main>
     <h2 class="admin-headnig">記事一覧</h2>
-    <a href="<?= ADMIN_ARTICLES_CREATE_URL ?>">
-      <button type="submit">新規作成</button>
+    <a href="<?= ADMIN_ARTICLES_CREATE_URL ?>" class="admin-create-new-btn">
+      新規作成
     </a>
     <div class="pagination">
       <a href="<?= $currentPage == 1 ? '#' : '?page=' . ($currentPage - 1) ?>" class="<?= $currentPage == 1 ? 'disabled' : '' ?>">前へ</a>
@@ -51,27 +51,44 @@ include GLOBAL_SHARED_PATH . '/head.php';
               <td><?= htmlspecialchars($article['created_at']) ?></td>
               <td><?= htmlspecialchars($article['updated_at']) ?></td>
               <td>
-                <?= htmlspecialchars($article['is_published'] == 1 ? '公開' : '非公開') ?>
-                <input type="checkbox" data-checkbox-id="<?= $article['id'] ?>" onchange="togglePublish(<?= htmlspecialchars($article['id']) ?>)" <?= $article['is_published'] == 1 ? "checked" : '' ?>>
+                <div class="toggle-container">
+                  <span class="toggle-text"><?= htmlspecialchars($article['is_published'] == 1 ? '公開' : '非公開') ?></span>
+                  <label class="toggle-switch">
+                    <input type="checkbox"
+                      data-checkbox-id="<?= $article['id'] ?>"
+                      onchange="togglePublish(<?= htmlspecialchars($article['id']) ?>)" <?= $article['is_published'] == 1 ? "checked" : '' ?>>
+                    <span class="slider"></span>
+                  </label>
+                </div>
               </td>
               <td>
                 <form action="/pj_homepage/admin/articles/edit" method="GET">
                   <input type="hidden" name="id" value="<?= htmlspecialchars($article['id'], ENT_QUOTES, 'UTF-8'); ?>">
-                  <button type="submit">編集</button>
+                  <button type="submit" class="admin-edit-btn">編集</button>
                 </form>
               </td>
               <td>
                 <form action="/pj_homepage/admin/articles/delete" method="POST">
                   <input type="hidden" name="article_id" value="<?php echo htmlspecialchars($article['id'], ENT_QUOTES, 'UTF-8'); ?>">
-                  <button type="submit">削除</button>
+                  <button type="submit" class="admin-delete-btn">削除</button>
                 </form>
               </td>
               <td>
-                <form method="POST" action="/pj_homepage/admin/articles/update_publish_date">
-                  <input type="datetime-local" id="publish-datetime" name="scheduled_publish_date">
-                  <input type="hidden" name="article_id" value="<?= htmlentities($article['id'], ENT_QUOTES, 'UTF-8'); ?>">
-                  <button type="submit">設定</button>
-                </form>
+                <?php if ($article['scheduled_publish_date']): ?>
+                  <span class="publish-date-display">
+                    <?= htmlspecialchars(date('Y-m-d H:i', strtotime($article['scheduled_publish_date']))) ?>
+                  </span>
+                <?php else: ?>
+                  <span class="publish-date-display">未設定</span>
+                <?php endif; ?>
+                <button type="button" class="publish-date-edit-btn" onclick="toggleScheduledPublishEdit(<?= $article['id'] ?>)">編集</button>
+                <div id="edit-form-<?= $article['id'] ?>" class="edit-form" style="display: none;">
+                  <form method="POST" action="/pj_homepage/admin/articles/update_publish_date">
+                    <input type="datetime-local" class="publish-date-input-datetime" id="publish-datetime" name="scheduled_publish_date" value="<?= $article['scheduled_publish_date'] ? htmlspecialchars($article['scheduled_publish_date']) : '' ?>">
+                    <input type="hidden" name="article_id" value="<?= htmlentities($article['id'], ENT_QUOTES, 'UTF-8'); ?>">
+                    <button type="submit" class="publish-date-save-btn">保存</button>
+                  </form>
+                </div>
               </td>
             </tr>
           <?php endforeach; ?>
@@ -82,6 +99,7 @@ include GLOBAL_SHARED_PATH . '/head.php';
 
   <footer>
     <script src="/pj_homepage/assets/js/admin/toggle_publish.js"></script>
+    <script src="/pj_homepage/assets/js/admin/scheduled-publish-editor.js"></script>
   </footer>
 </body>
 
