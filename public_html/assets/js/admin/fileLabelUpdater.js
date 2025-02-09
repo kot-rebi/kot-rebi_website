4fb976b23188532673d8a826254d1cd86e05d294
kot-rebi <kotrebi9@gmail.com>
2025-02-09 17:27:07 +0900

    .gitignoreファイルの更新

--- public_html/assets/js/admin/fileLabelUpdater.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('input[type="file"]').forEach((input) => {
-  const label = input.previousElementSibling;
-  input.addEventListener('change', (event) => {
-    const fileName = event.target.files[0]?.name || "ファイルを選択";
-    label.textContent = fileName;
-  });
-});
