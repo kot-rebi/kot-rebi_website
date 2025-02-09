00281fd4ff274e66640b3dfd3251fab426eb71d3
kot-rebi <kotrebi9@gmail.com>
2025-02-09 17:10:17 +0900

    delete: ファイル構造の変化による.gitignoreの追加 #41

--- public_html/assets/js/admin/deleteButton.js
+++
@@ -1,8 +0,0 @@
-function confirmDeleteButton() {
-  try {
-    return confirm("本当に削除しますか？");
-  } catch (error) {
-    console.log("エラーが発生しました: ", error);
-    alert("問題が発生しました。もう一度お試しください(-_-)");
-  }
-}
--- public_html/assets/js/admin/scheduled-publish-editor.js
+++
@@ -1,5 +0,0 @@
-function toggleScheduledPublishEdit(id) {
-  const editForm = document.getElementById(`edit-form-${id}`);
-  const isHidden = editForm.style.display === "none";
-  editForm.style.display = isHidden ? "block" : "none";
-}
